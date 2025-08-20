0c4b2d113e034be292bc1d8da23f550f011b2c39
Zarin <226439417+Memore-is@users.noreply.github.com>
2025-08-20 17:09:01 -0400

    included the text only

---
+++ backstorylabel.gd
@@ -0,0 +1,28 @@
+extends Label
+
+var messages = ["Welcome, everyone.",
+	"Two years ago, a virus broke out.",
+	"They said it was lab-made, controllable.",
+	"Yet people started to get infected, slowly, one by one.",
+	"Fun days.",
+	"Fast forward to now.",
+	"Nearly everyone got infected, and only a handful managed to survive.",
+	"You happened to be one of them, and so did your sister.",
+	"However,",
+	"Your friends died,",
+	"Your family died,",
+	"Your neighbors died,",
+	"Your teachers died—",
+	"Basically everyone.",
+	"But those days are now only memories.",
+	"You have to think of the future.",
+	"You must find a way to survive.",
+	"For your sister— who lost everything at such a young age,",
+	"So now, when you find dropped leaflets of a place called “The Circus and Attractions”,",
+	"Claiming that a ship will be arriving to pick up possible survivors,",
+	"How could you let the opportunity pass?",
+	"Your first mission is to pack up and leave by tonight."]
+	
+	
+var msgindex = 0
+var intime = 2.0  #secondspossibly
