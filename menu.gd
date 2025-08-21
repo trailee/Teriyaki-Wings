b82c1ca75b36c167540302f0adf75f5abeb51dd2
trailee <tracyc08@icloud.com>
2025-08-21 03:45:01 -0400

    Completed Credits stuff
    - added a pg2 w usable UI
    - added swipe SFX

--- menu.gd
+++ menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var credits_ui: Control = $"Credits UI/Credits UI"
+@onready var pg_1: Control = $"Credits UI/Credits UI/Pg1"
+@onready var pg_2: Control = $"Credits UI/Credits UI/Pg2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,9 +18,12 @@ func _on_start_button_pressed() -> void:
 	$Button_Pressed_Sound.play()
 	get_tree().change_scene_to_file("res://backstorynarration.tscn")
 
+
 func _on_option_button_pressed() -> void:
 	$Button_Pressed_Sound.play()
 	credits_ui.show()
+	pg_2.hide()
+
 
 func _on_start_button_mouse_entered() -> void:
 	$Hover_Button.play()
@@ -27,6 +32,19 @@ func _on_start_button_mouse_entered() -> void:
 func _on_option_button_mouse_entered() -> void:
 	$Hover_Button.play()
 
+
 func _on_close_credits_pressed() -> void:
 	$Button_Pressed_Sound.play()
 	credits_ui.hide()
+
+
+func _on_to_pg_2_pressed() -> void:
+	$Swipe_Page.play()
+	pg_1.hide()
+	pg_2.show()
+
+
+func _on_to_pg_1_pressed() -> void:
+	$Swipe_Page.play()
+	pg_2.hide()
+	pg_1.show()
