20ed30dbd775c89dd12b270853599f368e2c121f
trailee <tracyc08@icloud.com>
2025-08-21 18:27:34 -0400

    Character Movement and Colliders
    - got Desmona to move properly w/ animations
    - added colliders so she can't walk over objects

--- Characters tscn/desdemona.gd
+++ Characters tscn/desdemona.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 100.0
+const SPEED = 75.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = direction * SPEED
 	move_and_slide()
 	
+	if Input.is_action_pressed("ui_left"):
+		animated_sprite.play("Des_WalkLeft")
+	elif Input.is_action_pressed("ui_right"):
+		animated_sprite.play("Des_WalkRight")
+	elif Input.is_action_pressed("ui_up"):
+		animated_sprite.play("Des_WalkBack")
+	elif Input.is_action_pressed("ui_down"):
+		animated_sprite.play("Des_WalkFront")
+	else: 
+		animated_sprite.play("Des_Idle")
