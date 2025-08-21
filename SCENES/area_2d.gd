1a0e82d5d0760f62ac2a5bfa53a0a39b7774e3d5
tabby-code <joannalime09@gmail.com>
2025-08-20 22:49:29 -0400

    Scene Changes (still need fix)

--- SCENES/area_2d.gd
+++ SCENES/area_2d.gd
@@ -1,16 +1,13 @@
 extends Area2D
-
-
-
 var enter=false
 
-func _on_body_entered(body: PhysicsBody2D):
+func _on_area_2d_body_enter(body: PhysicsBody2D):
 	enter=true
 
 
-func _on_body_exited(body: Node2D) :
+func _on_area_2d_body_exited(body: Node2D) :
 	enter=false
 func _process(delta):
 	if enter==true:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene("res://forest.tscn")
+			get_tree().change_scene_to_file("res://forest.tscn")
