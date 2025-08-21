86e96c2dab5486d6ad78938f53b99402802348ea
trailee <tracyc08@icloud.com>
2025-08-21 04:40:57 -0400

    Fixed main menu buttons still on w captions

--- menu.gd
+++ menu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var credits_ui: Control = $"Credits UI/Credits UI"
 @onready var pg_1: Control = $"Credits UI/Credits UI/Pg1"
 @onready var pg_2: Control = $"Credits UI/Credits UI/Pg2"
+@onready var start_button: Button = $VBoxContainer/StartButton
+@onready var option_button: Button = $VBoxContainer/OptionButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +25,8 @@ func _on_option_button_pressed() -> void:
 	$Button_Pressed_Sound.play()
 	credits_ui.show()
 	pg_2.hide()
+	option_button.hide()
+	start_button.hide()
 
 
 func _on_start_button_mouse_entered() -> void:
@@ -36,6 +40,8 @@ func _on_option_button_mouse_entered() -> void:
 func _on_close_credits_pressed() -> void:
 	$Button_Pressed_Sound.play()
 	credits_ui.hide()
+	option_button.show()
+	start_button.show()
 
 
 func _on_to_pg_2_pressed() -> void:
