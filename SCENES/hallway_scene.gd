a67dd7df68b26f3a3c89c82f5deafb2bf83cbf65
tabby-code <joannalime09@gmail.com>
2025-08-22 10:54:01 -0400

    adding scene changes in hallway

--- SCENES/hallway_scene.gd
+++ SCENES/hallway_scene.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if SceneManager.player:
-		add_child(SceneManager.player)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- nscenetrigger.gd
+++ nscenetrigger.gd
@@ -1,7 +1,9 @@
 class_name SNceneTrigger extends Area2D
 
-@export var connected_scene: String
-
+@export var connected_scene: String 
+var scene_folder="res://SCENES/"
 func _on_body_entered(body: Node2D) :
-	if body is Player:
-		SceneManager.change_scene(get_owner(), connected_scene)
+	var full_path=scene_folder+connected_scene+".tscn"
+	var scene_tree=get_tree()
+	scene_tree.change_scene_to_file(full_path)
+	
