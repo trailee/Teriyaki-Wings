3b82500613da2e26e4b0882e8c97e0c319151778
trailee <tracyc08@icloud.com>
2025-08-21 23:01:46 -0400

    Fixed scene changes

--- SCENES/treehouse_scene.gd
+++ SCENES/treehouse_scene.gd
@@ -1,7 +1,12 @@
 extends Node2D
+@onready var desdemona_2d: CharacterBody2D = $Background/Desdemona2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/instructions.dialogue"), "start")
 	return
+
+
+func _on_exit_body_entered(body: CharacterBody2D) -> void:
+	get_tree().change_scene_to_file("res://SCENES/forest.tscn")
