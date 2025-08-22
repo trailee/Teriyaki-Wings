dc5c1566170a7ac36bbcaa74170e6219c1c79511
Zarin <226439417+Memore-is@users.noreply.github.com>
2025-08-22 06:14:39 -0400

    scene manager
    explained in discord

--- nscenetrigger.gd
+++ nscenetrigger.gd
@@ -1,9 +1,7 @@
 class_name SNceneTrigger extends Area2D
 
 @export var connected_scene: String
-var scene_folder = "res://scenes/"
 
 func _on_body_entered(body: Node2D) :
-	var full_path = scene_folder + connected_scene + ".tscn"
-	var scene_tree = get_tree()
-	scene_tree.change_scene_to_file(full_path)
+	if body is Player:
+		SceneManager.change_scene(get_owner(), connected_scene)
