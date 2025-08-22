0ce9da91fed43d453dd25af87a3a29277e20d548
Zarin <226439417+Memore-is@users.noreply.github.com>
2025-08-21 23:33:06 -0400

    deck to hallway scene change

---
+++ scenetrigger.gd
@@ -0,0 +1,11 @@
+class_name SceneTrigger extends Area2D
+
+@export var connected_scene: String
+var scene_folder = "res://scenes/"
+
+func _on_body_entered(body: Node2D) :
+	var full_path = scene_folder + connected_scene + ".tscn"
+	var scene_tree = get_tree()
+	scene_tree.change_scene_to_file(full_path)
+	
+	
