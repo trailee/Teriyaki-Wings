f3e08ed41448ec7a44e5c2566911f870ec6b2740
trailee <tracyc08@icloud.com>
2025-08-22 20:28:32 -0400

    Revert "a"
    This reverts commit b0fd49fdc21e538e2fb5dd9c468dfe673cfd4e05, reversing
    changes made to 70dde9f449d5c52200aadfd18fbaa8dc476b1faf.

--- SCENES/hallway_scene.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if SceneManager.player:
-		add_child(SceneManager.player)
--- zoren_dead_count.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var z_deadcount = 0
