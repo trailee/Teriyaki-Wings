5693a0485198474246ef53c0571b4152df771a01
trailee <tracyc08@icloud.com>
2025-08-23 01:27:19 -0400

    Revert "commit."
    This reverts commit 05476dc4e1831b025dac89a4297c751dbbd92962.

--- state.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var shelves_pressed: bool = false
-var box_pressed: bool = false
-var chest_pressed: bool = false
-var first_aid_pressed: bool = false
-var middle_pressed: bool = false
-var treehouse_exit: bool = false
