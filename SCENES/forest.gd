6176ae235095939ddb3b1bb48d28093e443105c5
trailee <tracyc08@icloud.com>
2025-08-23 16:28:24 -0400

    changes
    - fixed character colliders + scaling
    - music changes when zorens are all dead
    - added des_treehouse to ruins scene

--- SCENES/forest.gd
+++ SCENES/forest.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 func _ready():
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/Forest.dialogue"), "start")
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func _on_dialogue_ended(_resource: DialogueResource) -> void:
+	print("dialogue ended")
 	if ZorenDeadCount.z_deadcount == 5:
 		get_tree().change_scene_to_file("res://SCENES/ruins.tscn")
--- Zorans/mob.gd
+++ Zorans/mob.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var health = 5
+@onready var forest_bgm: AudioStreamPlayer2D = $"../../Audio/Forest BGM"
+@onready var fight_forest_bgm: AudioStreamPlayer2D = $"../../Audio/Fight Forest BGM"
 
 @onready var player: Player = $"../Desdemona2D2"
 
@@ -14,4 +16,7 @@ func take_damage():
 	$AnimationPlayer.play("Zoren_Hurt")
 	if health == 0:
 		ZorenDeadCount.z_deadcount += 1
+		if ZorenDeadCount.z_deadcount == 5:
+			forest_bgm.play()
+			fight_forest_bgm.stop()
 		queue_free()
