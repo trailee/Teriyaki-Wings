12cf0cb4bac0ab61e581d770f659e701a6ba984b
trailee <tracyc08@icloud.com>
2025-08-23 21:26:05 -0400

    send

--- Dialogue/state.gd
+++ Dialogue/state.gd
@@ -7,4 +7,5 @@ var first_aid_pressed: bool = false
 var middle_pressed: bool = false
 var treehouse_exit: bool = false
 
+var ruin_talked: bool = false
 var ruin_exit: bool = false
--- SCENES/ruins.gd
+++ SCENES/ruins.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
 func _on_dialogue_ended(_resource: DialogueResource) -> void:
 	print("dialogue ended")
 	if State.ruin_exit == true:
