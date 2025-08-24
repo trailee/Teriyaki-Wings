758a6fe6e80266fcc9db811af95764a1526524e3
trailee <tracyc08@icloud.com>
2025-08-24 01:09:20 -0400

    part 9

---
+++ Day 1 Scenes fixed/day_1_part_9.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+func _on_dialogue_ended(_resource: DialogueResource) -> void:
+	if State.des_int_people == true:
+		get_tree().change_scene_to_file("res://Day 2 Scenes/cabin_day_2_scene.tscn")
+		print("not wokring")
