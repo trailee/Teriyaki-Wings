de05f8580ac78d8b96c1e74315077d3f47f82c4e
trailee <tracyc08@icloud.com>
2025-08-24 02:22:03 -0400

    d2p14

---
+++ Day 2 Scenes/day_2_part_14_part_b_scene.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+func _on_dialogue_ended(_resource: DialogueResource) -> void:
+	if State.des_int_people == true:
+		get_tree().change_scene_to_file("res://Day 2 Scenes/day_2_part_15.tscn")
+		print("not wokring")
